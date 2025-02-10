ceba30efc4bc2ea5f8eb99b43b560d8ba46db91d
Sandhi M.Shahid F <Shahidsandhi1786@gmail.com>
2025-02-10 10:04:08 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='HospitalManagementSystem',
+    version='2.7',
+    packages=['package'],
+    url='',
+    license='',
+    author='shahid khan',
+    author_email='shahidsandhi1786@gmail.com',
+    description='HospitalManagementSystem for college python course'
+)
